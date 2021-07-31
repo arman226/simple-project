945008a52562592da9e2d39617e6505309ead34c
Ragub0413 <ragubangelica13@gmail.com>
2021-07-31 14:31:37 -0700

    implementing design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import '../App.css'
+import {makeStyles,Container,Paper, Typography} from '@material-ui/core'
+import backgrounds from '../assets/eco-friendly.jpg'
 
 const Home = () => {
+  const classes = useStyles()
   return (
-  <div>Home</div>
+  <div className="homepage-background">
+  <Paper className={classes.paperContainer}> 
+      <Typography>HAKDOG</Typography>
+    </Paper>
+  </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+ paperContainer:{
+   backgroundImage: {backgrounds}
+ }
+}));
 
 export default Home;
