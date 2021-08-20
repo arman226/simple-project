8f7ca32fa5d2ab0e38a3214e6eb735d3d81c2e5a
Ragub0413 <ragubangelica13@gmail.com>
2021-08-19 20:08:29 -0700

    adding design and details

---
+++ src/components/HomepageComponents/Hero.js
@@ -0,0 +1,94 @@
+import React, { useRef, useState,useEffect } from 'react'
+import './Hero.css'
+import {COLORS} from '../../styles/color.styles'
+import {makeStyles} from '@material-ui/core'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+const Hero =({slides})=>{
+    const classes = useStyles()
+    const [current,setCurrent]= useState(0);
+    const length = slides.length
+    const timeout = useRef(null)
+
+    useEffect(()=>{
+        const nextSlide=()=>{
+            setCurrent(current=>(current=== length-1?0:current+1))
+        }
+        timeout.current =setTimeout(nextSlide,3000)
+        return function(){
+            if(timeout.current){
+                clearTimeout(timeout.current)
+            }
+        }
+    },[current,length])
+
+    const nextSlide =()=>{
+        setCurrent(current=== length -1?0:current+1)
+        console.log(current)
+    }
+    const prevSlide =()=>{
+        setCurrent(current=== 0?length -1:current-1);
+        console.log(current)
+    }
+    if(!Array.isArray(slides)|| slides.length<=0){
+        return null
+    }
+
+    return(
+       <div className="HeroSection">
+           <div className="HeroWrapper">
+             {slides.map((slide,index)=>{
+               return(
+                <div className="HeroSlide" key={index} >
+                {index === current &&(
+                        <div className="HeroSlider">
+                            <img src={slide.image } className="HeroImage"></img>
+                        <div className="HeroContent">
+                           <h1 className={classes.Heroh1} >{slide.title}</h1>
+                           <p>{slide.details}</p>
+                           </div>
+                       </div>
+                )}
+              </div>
+             )})}
+             <div className="SliderButtons">
+                 <ArrowBackIosIcon  style={{ fontSize:'clamp(3rem,6vw,3rem)', color: COLORS.GREEN2 }}onClick={prevSlide}/>
+                 <ArrowForwardIosIcon  style={{ fontSize:'clamp(3rem,6vw,3rem)', color: COLORS.GREEN2  }}onClick={nextSlide}/>
+             </div>
+           </div>
+       </div>
+    )
+}
+const useStyles = makeStyles((theme)=>({
+    HeroContent:{
+        zIndex: 4,
+        height: 'calc(100vh - 80px)',
+        maxHeight: '100%',
+        padding: '0rem calc((100vw - 1300px)/2)'
+    },
+    HeroItems:{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems:'center',
+        textAlign: 'center',
+        height: '100vh',
+        maxHeight: '100%',
+        padding: 0,
+        color: '#fff',
+        lineHeight: 1.1,
+        fontWeight: 'bold',
+        backgroundColor: '#0c8w9s'
+    },
+    Heroh1:{
+        fontSize: 'clamp(2rem,8vw,6rem)',
+        marginBottom: '1.5rem',
+        letterSpacing: '3px',
+        padding: '0 1rem',
+        fontWeight:'bolder',
+        color: COLORS.YELLOW2,
+        textShadow: '2px 2px #000'
+    },
+}))
+export default Hero
---
+++ src/components/HomepageComponents/SliderData.js
@@ -0,0 +1,25 @@
+import background from '../../assets/image/photo2.jpg'
+import background2 from '../../assets/image/eco-friendly.jpg'
+import mountain from '../../assets/image/photo3.jpg'
+
+export const SliderData=[
+    {
+        title: 'REDUCE',
+        path: '/',
+        image: mountain
+    },
+    {
+        title: 'REUSE',
+        path: '/',  
+        image: background2
+    },
+  
+    {
+        title: 'RECYCLE',
+        path: '/',
+        image: background,
+        alt: 'back',
+    },
+   
+   
+]
